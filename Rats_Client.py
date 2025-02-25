a9dde833e8bdad93b5c6c773d0ec7b2b04e8e341
William Faircloth <89104800+Kidtruck91@users.noreply.github.com>
2025-02-25 09:16:12 -0500

    Update Rats_Client.py

--- Rats_Client.py
+++ Rats_Client.py
@@ -2,7 +2,7 @@ import socket
 import pickle
 from game_logic import Game, Player
 
-SERVER_HOST = "127.0.0.1"  # Use localhost or adjust for remote play
+SERVER_HOST = "192.168.1.7"  # Use localhost or adjust for remote play
 SERVER_PORT = 5555
 
 def main():
